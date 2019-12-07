5e2136f534b4aff0293f61661b1eb5a37f9b6b40
Asif Kamal <asif_kamal@hotmail.com>
2019-12-07 22:31:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,12 +8,13 @@ def directors_totals(nds)
 directors_gross_total = {}
 row_index = 0
 while row_index < nds.length do
-  directors_gross_total[:name] = nds[row_index][:name]
+  directors_name = nds[row_index][:name]
+  directors_gross_total[directors_name] = 0
   column_index = 0
   #binding.pry
   while column_index < nds[row_index][:movies].length do
   #binding.pry
-  directors_gross_total[nds[row_index][:name]] += nds[row_index][:movies][column_index][:worldwide_gross]
+  directors_gross_total[directors_name] += nds[row_index][:movies][column_index][:worldwide_gross]
   
   column_index += 1 
 end
