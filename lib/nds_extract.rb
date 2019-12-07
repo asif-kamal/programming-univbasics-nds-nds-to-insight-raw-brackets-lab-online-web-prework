d56c94ae04cfd6c1bc142bfeefde4866f187b3ff
Asif Kamal <asif_kamal@hotmail.com>
2019-12-07 22:24:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,7 +12,7 @@ while row_index < nds.length do
   column_index = 0
   #binding.pry
   while column_index < nds[row_index][:movies].length do
-  binding.pry
+  #binding.pry
   directors_gross_total[nds[row_index][:name]] += nds[row_index][:movies][column_index][:worldwide_gross]
   
   column_index += 1 
